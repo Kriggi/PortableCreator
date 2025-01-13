4ef2aa1ee518195ea6e8bf936ce904f7d36c2a75
Kriggi <leserg@ua.fm>
2025-01-13 19:25:51 +0100

    Add files via upload

---
+++ PortableCreator/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace PortableCreator
+{
+	public partial class Form1 : System.Windows.Forms.Form
+	{
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && this.components != null)
+			{
+				this.components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.createBtn = new System.Windows.Forms.Button();
+			this.projectName = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtSource = new System.Windows.Forms.TextBox();
+			this.txtStatus = new System.Windows.Forms.TextBox();
+			this.exePath = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.clearLogs = new System.Windows.Forms.CheckBox();
+			this.customArgs = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label4 = new System.Windows.Forms.Label();
+			this.drvsExport = new System.Windows.Forms.Button();
+			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// createBtn
+			// 
+			this.createBtn.Location = new System.Drawing.Point(208, 186);
+			this.createBtn.Name = "createBtn";
+			this.createBtn.Size = new System.Drawing.Size(162, 36);
+			this.createBtn.TabIndex = 0;
+			this.createBtn.Text = "Создать";
+			this.createBtn.UseVisualStyleBackColor = true;
+			this.createBtn.Click += new System.EventHandler(this.CreateBtn_Click);
+			// 
+			// projectName
+			// 
+			this.projectName.Location = new System.Drawing.Point(92, 13);
+			this.projectName.Name = "projectName";
+			this.projectName.Size = new System.Drawing.Size(255, 20);
+			this.projectName.TabIndex = 1;
+			this.toolTip1.SetToolTip(this.projectName, "Введите имя проекта или название приложения.");
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(9, 16);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(79, 13);
+			this.label1.TabIndex = 12;
+			this.label1.Text = "Имя проекта :";
+			// 
+			// txtSource
+			// 
+			this.txtSource.Location = new System.Drawing.Point(98, 225);
+			this.txtSource.Multiline = true;
+			this.txtSource.Name = "txtSource";
+			this.txtSource.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.txtSource.Size = new System.Drawing.Size(79, 36);
+			this.txtSource.TabIndex = 15;
+			this.txtSource.TabStop = false;
+			this.txtSource.Text = resources.GetString("txtSource.Text");
+			this.txtSource.Visible = false;
+			// 
+			// txtStatus
+			// 
+			this.txtStatus.Location = new System.Drawing.Point(15, 225);
+			this.txtStatus.Multiline = true;
+			this.txtStatus.Name = "txtStatus";
+			this.txtStatus.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.txtStatus.Size = new System.Drawing.Size(73, 36);
+			this.txtStatus.TabIndex = 16;
+			this.txtStatus.TabStop = false;
+			this.txtStatus.Visible = false;
+			// 
+			// exePath
+			// 
+			this.exePath.Location = new System.Drawing.Point(92, 49);
+			this.exePath.Name = "exePath";
+			this.exePath.Size = new System.Drawing.Size(255, 20);
+			this.exePath.TabIndex = 2;
+			this.toolTip1.SetToolTip(this.exePath, "Полный путь к исполняемому файлу приложения.");
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(9, 52);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(73, 13);
+			this.label2.TabIndex = 13;
+			this.label2.Text = "Путь к *.exe :";
+			// 
+			// clearLogs
+			// 
+			this.clearLogs.AutoSize = true;
+			this.clearLogs.Location = new System.Drawing.Point(133, 81);
+			this.clearLogs.Name = "clearLogs";
+			this.clearLogs.Size = new System.Drawing.Size(214, 17);
+			this.clearLogs.TabIndex = 3;
+			this.clearLogs.Text = "Очищать папку логов после работы?";
+			this.toolTip1.SetToolTip(this.clearLogs, "Будут удалены папки \"C:\\Users\\Имя\\AppData\\Local\\Microsoft\\CLR*\".");
+			this.clearLogs.UseVisualStyleBackColor = true;
+			// 
+			// customArgs
+			// 
+			this.customArgs.Location = new System.Drawing.Point(92, 108);
+			this.customArgs.Name = "customArgs";
+			this.customArgs.Size = new System.Drawing.Size(255, 20);
+			this.customArgs.TabIndex = 4;
+			this.toolTip1.SetToolTip(this.customArgs, "Параметры командной строки для запуска приложения, если нужно.");
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(9, 111);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(69, 13);
+			this.label3.TabIndex = 14;
+			this.label3.Text = "Аргументы :";
+			// 
+			// panel1
+			// 
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel1.Controls.Add(this.customArgs);
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Controls.Add(this.projectName);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.clearLogs);
+			this.panel1.Controls.Add(this.exePath);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Location = new System.Drawing.Point(12, 29);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(358, 141);
+			this.panel1.TabIndex = 10;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 11);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(129, 13);
+			this.label4.TabIndex = 11;
+			this.label4.Text = "Параметры загрузчика:";
+			// 
+			// drvsExport
+			// 
+			this.drvsExport.Location = new System.Drawing.Point(12, 186);
+			this.drvsExport.Name = "drvsExport";
+			this.drvsExport.Size = new System.Drawing.Size(162, 36);
+			this.drvsExport.TabIndex = 5;
+			this.drvsExport.Text = "Экспорт драйверов";
+			this.drvsExport.UseVisualStyleBackColor = true;
+			this.drvsExport.Click += new System.EventHandler(this.DrvsExport_Click);
+			// 
+			// toolTip1
+			// 
+			this.toolTip1.IsBalloon = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(382, 232);
+			this.Controls.Add(this.drvsExport);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.txtStatus);
+			this.Controls.Add(this.txtSource);
+			this.Controls.Add(this.createBtn);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Opacity = 0.97D;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "PortableCreator";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		private System.ComponentModel.IContainer components;
+		private System.Windows.Forms.Button createBtn;
+		private System.Windows.Forms.TextBox projectName;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtSource;
+		private System.Windows.Forms.TextBox txtStatus;
+		private System.Windows.Forms.TextBox exePath;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.CheckBox clearLogs;
+		private System.Windows.Forms.TextBox customArgs;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button drvsExport;
+		private System.Windows.Forms.ToolTip toolTip1;
+		}
+}
---
+++ PortableCreator/Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Windows.Forms;
+using PortableCreator.Properties;
+using TsudaKageyu;
+
+namespace PortableCreator
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			this.InitializeComponent();
+		}
+
+		private void CreateBtn_Click(object sender, EventArgs e)
+		{
+			this.sourceCode = this.txtSource.Text;
+			if (this.projectName.Text.Length < 1 || this.exePath.Text.Length < 3)
+			{
+				MessageBox.Show("Имя проекта или Путь к основному *.exe файлу приложения не могут быть пустыми.", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				return;
+			}
+			this.txtSource.Text = this.txtSource.Text.Replace("Text = \"Form1\";", "Text = \"" + ((this.projectName.Text.Length < 1) ? "Generated" : this.projectName.Text) + "\";");
+			this.txtSource.Text = this.txtSource.Text.Replace("\"PortableCreator\"", "\"" + ((this.projectName.Text.Length < 1) ? "Generated" : this.projectName.Text) + "\"");
+			this.CheckData();
+			this.txtSource.Text = this.sourceCode;
+		}
+
+		private void CheckData()
+		{
+			if (this.exePath.Text.Contains("\\Program Files\\"))
+			{
+				this.exePathDone = this.exePath.Text.Replace("\"", "").Replace(this.appPath + "Data\\Program Files", "C:\\Program Files");
+				if (!this.exePathDone.EndsWith(".exe"))
+				{
+					this.exePathDone += ".exe";
+				}
+				if (!File.Exists(this.exePathDone) && !File.Exists(this.exePath.Text.Replace("\"", "")))
+				{
+					MessageBox.Show("Этот .exe файл отсутствует по указанному пути:\r\n\"" + this.exePathDone + "\"", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+					return;
+				}
+				this.txtSource.Text = this.txtSource.Text.Replace("appPath + @\"\\EXEPATH\"", "@\"" + this.exePathDone + "\"");
+			}
+			else if (this.exePath.Text.Contains("\\Program Files (x86)\\"))
+			{
+				this.exePathDone = this.exePath.Text.Replace("\"", "").Replace(this.appPath + "Data\\Program Files (x86)", "C:\\Program Files (x86)");
+				if (!this.exePathDone.EndsWith(".exe"))
+				{
+					this.exePathDone += ".exe";
+				}
+				if (!File.Exists(this.exePathDone) && !File.Exists(this.exePath.Text.Replace("\"", "")))
+				{
+					MessageBox.Show("Этот .exe файл отсутствует по указанному пути:\r\n\"" + this.exePathDone + "\"", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+					return;
+				}
+				this.txtSource.Text = this.txtSource.Text.Replace("appPath + @\"\\EXEPATH\"", "@\"" + this.exePathDone + "\"");
+			}
+			else
+			{
+				this.exePathDone = this.exePath.Text.Replace("\"", "").Replace(this.appPath, "");
+				if (!this.exePathDone.EndsWith(".exe"))
+				{
+					this.exePathDone += ".exe";
+				}
+				if (!File.Exists(this.exePathDone) && !File.Exists(this.exePath.Text.Replace("\"", "")))
+				{
+					MessageBox.Show("Этот .exe файл отсутствует по указанному пути:\r\n\"" + this.exePathDone + "\"", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+					return;
+				}
+				this.txtSource.Text = this.txtSource.Text.Replace("EXEPATH", this.exePathDone);
+				if (File.Exists(exePathDone))
+				{
+					FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(this.exePathDone);
+					string exeVersion = versionInfo.FileVersion;
+					if (exeVersion != "")
+					{
+						this.txtSource.Text = this.txtSource.Text.Replace("\"2.4.0.0\"", "\"" + exeVersion + "\"");
+					}
+				}
+			}
+			if (this.clearLogs.Checked)
+			{
+				this.txtSource.Text = this.txtSource.Text.Replace("//ClearLogs", "try { Directory.Delete(userPath + @\"\\AppData\\Local\\Microsoft\\CLR_v4.0\", true); Directory.Delete(userPath + @\"\\AppData\\Local\\Microsoft\\CLR_v4.0_32\", true); } catch {}");
+			}
+			if (this.customArgs.Text.Length > 1)
+			{
+				string text = this.customArgs.Text.Replace("\"", "\\\"");
+				this.txtSource.Text = this.txtSource.Text.Replace(", ar);", ", \"" + text + " \" + ar);");
+			}
+			this.Generate();
+		}
+
+		private bool IsDirectoryEmpty(string path)
+		{
+			return !Directory.EnumerateFileSystemEntries(path).Any<string>();
+		}
+
+		private void Generate()
+		{
+			using (FileStream fileStream = new FileStream(this.appPath + "icon.ico", FileMode.CreateNew))
+			{
+				try
+				{
+					if (!File.Exists(this.exePathDone))
+					{
+						new IconExtractor(this.exePath.Text.Replace("\"", "")).GetIcon(0).Save(fileStream);
+					}
+					else
+					{
+						new IconExtractor(this.exePathDone).GetIcon(0).Save(fileStream);
+					}
+				}
+				catch
+				{
+				}
+			}
+			this.txtStatus.Clear();
+			using (ResXResourceWriter resXResourceWriter = new ResXResourceWriter("StringResource.resx"))
+			{
+				resXResourceWriter.AddResource("executablePath\u200e", this.exePathDone);
+				resXResourceWriter.AddResource("executableArgs\u200e", this.customArgs.Text);
+			}
+			string text = this.appPath + ((this.projectName.Text.Length < 1) ? "Generated" : this.projectName.Text) + ".exe";
+			string[] array = new string[] { "System.dll", "System.Drawing.dll", "System.Windows.Forms.dll" };
+			CodeDomProvider codeDomProvider = CodeDomProvider.CreateProvider("CSharp");
+			CompilerParameters compilerParameters = new CompilerParameters(array, "");
+			compilerParameters.ReferencedAssemblies.Add("System.Linq.dll");
+			compilerParameters.ReferencedAssemblies.Add("System.Core.dll");
+			compilerParameters.ReferencedAssemblies.Add("Microsoft.CSharp.dll");
+			compilerParameters.ReferencedAssemblies.Add("System.Runtime.InteropServices.dll");
+			string text2 = this.appPath + "System.Management.Automation.dll";
+			File.WriteAllBytes(text2, Resources.System_Management_Automation);
+			compilerParameters.ReferencedAssemblies.Add("System.Management.Automation.dll");
+			compilerParameters.OutputAssembly = text;
+			compilerParameters.GenerateExecutable = true;
+			compilerParameters.GenerateInMemory = true;
+			compilerParameters.WarningLevel = 3;
+			compilerParameters.TreatWarningsAsErrors = true;
+			compilerParameters.EmbeddedResources.Add("StringResource.resx");
+			if (File.Exists(this.appPath + "icon.ico"))
+			{
+				if (new FileInfo(this.appPath + "icon.ico").Length == 0L)
+				{
+					compilerParameters.CompilerOptions = "/optimize /target:winexe";
+					File.Delete(this.appPath + "icon.ico");
+				}
+				else
+				{
+					compilerParameters.CompilerOptions = "/optimize /target:winexe /win32icon:\"" + this.appPath + "icon.ico\"";
+				}
+			}
+			string text3 = null;
+			try
+			{
+				CompilerResults compilerResults = codeDomProvider.CompileAssemblyFromSource(compilerParameters, new string[] { this.txtSource.Text });
+				if (compilerResults.Errors.Count > 0)
+				{
+					text3 = "";
+					foreach (object obj in compilerResults.Errors)
+					{
+						CompilerError compilerError = (CompilerError)obj;
+						text3 = string.Concat(new string[]
+						{
+							text3,
+							"Line number ",
+							compilerError.Line.ToString(),
+							", Error Number: ",
+							compilerError.ErrorNumber,
+							", '",
+							compilerError.ErrorText,
+							";\r\n\r\n"
+						});
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				text3 = ex.Message;
+			}
+			if (text3 == null)
+			{
+				this.txtStatus.Text = "\n\r" + text + " Compiled !";
+				File.Delete(this.appPath + "icon.ico");
+				if (File.Exists(this.appPath + "log.txt"))
+				{
+					File.Delete(this.appPath + "log.txt");
+				}
+				string text4 = this.appPath + "crc.exe";
+				File.WriteAllBytes(text4, Resources.crc);
+				Process process = new Process
+				{
+					StartInfo = 
+					{
+						FileName = "crc",
+						WorkingDirectory = this.appPath,
+						Arguments = string.Concat(new string[]
+						{
+							"\"",
+							this.appPath,
+							"\\",
+							this.projectName.Text,
+							".exe\""
+						}),
+						CreateNoWindow = true,
+						UseShellExecute = false
+					}
+				};
+				process.Start();
+				process.WaitForExit();
+				File.Delete(text4);
+				File.Delete(text2);
+				File.Delete(this.appPath + "StringResource.resx");
+				MessageBox.Show("Лаунчер успешно создан!", "Готово!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+			else
+			{
+				this.txtStatus.Text = "Error occurred during compilation : \r\n" + text3;
+				File.Delete(this.appPath + "icon.ico");
+				MessageBox.Show(this.txtStatus.Text, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+			this.txtSource.Text = this.sourceCode;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			new List<string>
+			{
+				this.appPath + "App",
+				this.appPath + "Data\\AppData\\Local",
+				this.appPath + "Data\\AppData\\LocalLow",
+				this.appPath + "Data\\AppData\\Roaming",
+				this.appPath + "Data\\ProgramData",
+				this.appPath + "Data\\Program Files",
+				this.appPath + "Data\\Program Files (x86)",
+				this.appPath + "Data\\Program Files (x86)\\Common Files",
+				this.appPath + "Data\\Program Files\\Common Files",
+				this.appPath + "Data\\Пользователи\\Видео",
+				this.appPath + "Data\\Пользователи\\Документы",
+				this.appPath + "Data\\Пользователи\\Общие\\Документы",
+				this.appPath + "Data\\Пользователи\\Загрузки",
+				this.appPath + "Data\\Пользователи\\Изображения",
+				this.appPath + "Data\\Пользователи\\Музыка",
+				this.appPath + "Data\\Пользователи\\Рабочий стол",
+				this.appPath + "Data\\Реестр",
+				this.appPath + "Data\\Драйвер"
+			}.AsParallel<string>().ForAll(delegate(string x)
+			{
+				if (!Directory.Exists(x))
+				{
+					Directory.CreateDirectory(x);
+				}
+			});
+			new List<string>
+			{
+				this.appPath + "Data\\Закрыть.txt",
+				this.appPath + "Data\\Реестр\\Удалить.txt",
+				this.appPath + "Data\\Проводник.txt",
+				this.appPath + "Data\\Кастом.txt",
+				this.appPath + "Data\\Службы.txt"
+			}.AsParallel<string>().ForAll(delegate(string x)
+			{
+				if (!File.Exists(x))
+				{
+					this.WriteAsStream(x, "", false);
+				}
+			});
+		}
+
+		private void WriteAsStream(string path, string data, bool append)
+		{
+			if (append)
+			{
+				using (StreamWriter streamWriter = new StreamWriter(path, true))
+				{
+					streamWriter.WriteLine(data, Encoding.UTF8);
+					return;
+				}
+			}
+			using (StreamWriter streamWriter2 = new StreamWriter(path))
+			{
+				streamWriter2.Write(data, Encoding.UTF8);
+			}
+		}
+
+		private void DrvsExport_Click(object sender, EventArgs e)
+		{
+			using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+			{
+				fbd.Description = "Выберите папку, которую необходимо использовать, или создайте новую:"; 
+				if (fbd.ShowDialog() == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+				{
+					List<string> list = Directory.GetFiles(fbd.SelectedPath).ToList<string>();
+					List<string> findedFiles = new List<string>();
+					list.ForEach(delegate(string x)
+					{
+						if (x.EndsWith("Список установленных программ.htm") | x.EndsWith("Installed programs.htm") | x.EndsWith("Liste der installierten Programme.htm") | x.EndsWith("Список встановлених програм.htm") | x.EndsWith("Custom Windows Settings.reg"))
+						{
+							if (x.EndsWith(".reg"))
+							{
+								findedFiles.Add(x);
+							}
+							if (x.EndsWith(".htm"))
+							{
+								string text = File.ReadAllText(x);
+								if (text.StartsWith("<html><head><meta") | text.EndsWith("<br></div><br><br><br></body></html>"))
+								{
+									findedFiles.Add(x);
+								}
+							}
+						}
+					});
+					if (findedFiles.LongCount<string>() == 2L)
+					{
+						List<string> list2 = Directory.GetDirectories(fbd.SelectedPath).ToList<string>();
+						if (!Directory.Exists(this.desktopPath + "\\Drivers for portable"))
+						{
+							Directory.CreateDirectory(this.desktopPath + "\\Drivers for portable");
+						}
+						Process process = new Process();
+						process.StartInfo.FileName = "cmd";
+						process.StartInfo.Arguments = "/C pnputil /export-driver * \"" + this.desktopPath + "\\Drivers for portable\"";
+						process.StartInfo.CreateNoWindow = true;
+						process.StartInfo.UseShellExecute = false;
+						process.Start();
+						process.WaitForExit();
+						list2.ForEach(delegate(string x)
+						{
+							x = x.Replace(fbd.SelectedPath, this.desktopPath + "\\Drivers for portable");
+							if (Directory.Exists(x))
+							{
+								Directory.Delete(x, true);
+							}
+						});
+						MessageBox.Show("Папка с драйверами на рабочем столе", "Готово!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+					}
+					else
+					{
+						MessageBox.Show("Папка драйверов Win 10 Tweaker имеет неверный формат.\r\nПопробуйте создать новый экспорт и выбрать его.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					}
+				}
+			}
+		}
+
+		private string appPath = AppDomain.CurrentDomain.BaseDirectory;
+		private string userPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+		private string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+		private string exePathDone = "";
+		private string sourceCode = "";
+	}
+}
